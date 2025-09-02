c91eb136b55fb85909c130c07501d6d22cb20543
KenDnG <daniswara.kenrick@gmail.com>
2025-09-02 09:43:32 +0700

    Add project files.

---
+++ Domain/Models/University.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    [Table ("Tbl_University")]
+    public class University
+    {
+        public Guid UniversityId { get; set; }
+        public string? Domains { get; set; }
+        public string? WebPages {  get; set; }
+        public string? Name { get; set; }
+        public string? CountryCode { get; set; }
+        public string? StateProvince { get; set; }
+    }
+}
---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<University> Universities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<University>(entity =>
+            {
+                entity.HasKey(e => e.UniversityId);
+            });
+        }
+    }
+}
